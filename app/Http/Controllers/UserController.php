3499def627541e513fbea90c6ea68fc8f733482e
anup-paul <anupdesktopit@gmail.com>
2021-07-14 13:24:54 +0600

    create_signup_and_loging_api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Redirect,Response,File;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,7 +16,38 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('ckeck');
+        return view('Check');
+    }
+
+
+    function register(Request $req)
+    {
+        $name = $req->input('name');
+        $email = $req->input('email');
+        $password = Hash::make($req->input('password'));
+
+        DB::table('users')->insert([
+           'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+    }
+
+
+    function login(Request $req)
+    {
+        $email = $req->input('email');
+        $password = $req->input('password');
+
+        $user = DB::table('users')->where('email',$email)->first();
+        if (!Hash::check($password, $user->password))
+        {
+            echo 'Not Matched';
+        }
+        else
+        {
+            echo $user->email;
+        }
     }
 
     /**
